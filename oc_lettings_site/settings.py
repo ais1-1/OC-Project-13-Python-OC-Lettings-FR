ae20581421e50d68b0168fb3d56f1c3332a662bf
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-11-29 16:31:30 +0100

    Add sentry config to settings and more
    -Add sentry dsn environment variable example to dist file
    -Update requirements

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,8 @@
 import os
 import environ
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
 from pathlib import Path
 
 
@@ -131,3 +134,16 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Sentry configuration
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
