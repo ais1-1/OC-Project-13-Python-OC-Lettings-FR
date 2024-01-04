54a77605c0ca6648c066272b169677f440c3c38f
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2024-01-04 14:23:38 +0100

    Test pipeline fail on linting error

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.http import require_safe
 
 @require_safe
 def home(request):
-    """Renders home page"""
+    """Renders home pagebbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"""
     return render(request, "home/home.html")
 
 
