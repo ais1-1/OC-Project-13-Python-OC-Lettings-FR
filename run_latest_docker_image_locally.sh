6e5ba0d8179e6debeb15d4df8ef95af9b37a6b25
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2024-01-10 14:32:55 +0100

    Add help script to pull latest docker image and run locally

---
+++ run_latest_docker_image_locally.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+docker_image_name=${1:-a15h/orange-county-lettings-oc-13:latest}
+
+docker_username=$(grep "DOCKER_USER" .env | cut -d "=" -f2-)
+docker_password=$(grep "DOCKER_PASSWORD" .env | cut -d "=" -f2-)
+port=$(grep "PORT" .env | cut -d "=" -f2-)
+container_name="OC-lettings"
+
+echo "Connecting to Docker..."
+docker login --username $docker_username --password $docker_password
+
+if [ $? -eq 0 ]; then
+    echo "Successfully connected."
+else
+    echo "Failed to connect to docker hub. Please verify your credentials."
+    exit 1   
+fi
+
+echo "Downloading docker image..."
+docker pull $docker_image_name
+
+if [ $? -eq 0 ]; then
+    echo "The docker image has successfully downloaded."
+else
+    echo "The download of the docker image has failed. Please verify your repository name."
+    exit 1
+fi
+
+echo "Running a docker container..."
+if [ ! "$(docker ps -a -q -f name=$container_name)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
+        # cleanup
+        docker rm $container_name
+    fi
+    # run the container
+    docker run --env-file .env --name $container_name -p $port:$port -it -d $docker_image_name
+fi
+
+
+if [ $? -eq 0 ]; then
+    echo "The docker container is running successfully."
+    echo "You can access the site at : http://0.0.0.0:$port/"
+else
+    echo "The docker container couldn't be run."
+    exit 1
+fi
